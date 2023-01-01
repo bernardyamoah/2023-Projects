cafd4dc930d4f8b9114d4a434a502d85f48326bb
Bernard Yamoah <bernardyamoah2016@gmail.com>
2023-01-01 15:38:28 +0000

    update

---
+++ links/app.js
@@ -0,0 +1,19 @@
+const shareButtons = document.querySelectorAll('.tile-share-button')
+console.log(shareButtons)
+
+
+
+async function copyText(e){
+    e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
+    try{
+    await navigator.clipboard.writeText(link)
+    alert('copied the text:' + link)
+    }
+    catch(err){
+    console.log(err)
+    }
+}
+
+shareButtons.forEach(shareButton=> shareButton.addEventListener('click', copyText))
