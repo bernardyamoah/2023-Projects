3239ca1d3476d3870e015c73e1e2e5ee1a3742e3
Bernard Yamoah <bernardyamoah2016@gmail.com>
2023-01-05 10:20:36 +0000

    nofification added

--- links/app.js
+++ links/app.js
@@ -1,19 +1,19 @@
+
 const shareButtons = document.querySelectorAll('.tile-share-button')
-console.log(shareButtons)
 
 
-
+// var notification = alertify.notify(link, 'success', 5, shareButtons.forEach(shareButton=> shareButton.addEventListener('click', copyText)));
 async function copyText(e){
     e.preventDefault()
     const link = this.getAttribute('link')
     console.log(link)
     try{
     await navigator.clipboard.writeText(link)
-    alert('copied the text:' + link)
+    swal("Good job!", link , "success")
     }
     catch(err){
     console.log(err)
     }
 }
 
-shareButtons.forEach(shareButton=> shareButton.addEventListener('click', copyText))
+ shareButtons.forEach(shareButton=> shareButton.addEventListener('click', copyText))
