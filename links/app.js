3ba6fb5ff46f289914e7c75a6146d89eb57e7b3d
Bernard Yamoah <bernardyamoah2016@gmail.com>
2023-01-05 12:23:06 +0000

    nofification added

--- links/app.js
+++ links/app.js
@@ -2,21 +2,31 @@
 const shareButtons = document.querySelectorAll('.tile-share-button')
 
 
-// var notification = alertify.notify(link, 'success', 5, shareButtons.forEach(shareButton=> shareButton.addEventListener('click', copyText)));
+
 async function copyText(e){
     e.preventDefault()
     const link = this.getAttribute('link')
     console.log(link)
     try{
     await navigator.clipboard.writeText(link)
-    // swal("Good job!", link , "success")
-    iziToast.show({
-    title: 'Hey',
-    message: link
-});
+    swal("Good job!","Link has been successfully copied!", "success")
+
+    // // Izi toast
+    // iziToast.success({
+    //         title: 'Success',
+    //         message: 'Successfully copied!',
+    //     overlay: true,
+        
+    //     zindex: 999,
+    //     });
+
     }
     catch(err){
-    console.log(err)
+        // iziToast.error({
+        //     title: 'Error',
+        //     message: err,
+        // });
+        swal ( "Oops" , err,  "error" )
     }
 }
 
