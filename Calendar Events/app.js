d4ba4d75d32c9b2a06d3c9e423bb282407a673d9
Bernard Yamoah <bernardyamoah2016@gmail.com>
2023-02-18 21:15:54 +0000

    Calendar Events

--- Calendar Events/app.js
+++ Calendar Events/app.js
@@ -1,5 +1,85 @@
 
-const shareButtons = document.querySelectorAll('.tile-share-button')
+const calendar= document.getElementById('calendar')
+const monthEl = document.getElementById('month');
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+
+
+const drawBlankCalendar=()=>{
+    for (let index = 0; index < 35; index++) {
+        const day=document.createElement('div');
+        day.classList.add('day','group');
+
+        const dayText= document.createElement('span');
+        dayText.classList.add('day-text');//,'group-hover:bg-purple-300','group-hover:text-purple-600'
+        dayText.innerText=days[index%7]
+
+        const dayNumber= document.createElement('p');
+        dayNumber.classList.add('day-number');
+
+        const eventName= document.createElement('small')
+        eventName.classList.add('event-name')
+
+        day.appendChild(dayText)
+        day.appendChild(dayNumber)  
+        day.appendChild(eventName)
+
+        console.log(day)
+        calendar.appendChild(day)
+        
+    }
+}
+
+const updateCalendar= (month,year,events)=>{
+    const dayElements= document.querySelectorAll('.day')
+    const theFirst= new Date();
+    theFirst.setMonth(month)
+    theFirst.setYear(year)
+
+    const theFirstDayOfWeek= theFirst.getDay();
+    const monthName=months[month]
+    const monthWithYear=   `${year} - ${month}`;
+    // days within a month
+    const daysInMonth=new Date(year,month+1,0).getDate()
+    
+    for (let i=0; i<dayElements.length;i++){
+        const day=dayElements[i]
+
+        
+    }
+}
+
+
+drawBlankCalendar()
+updateCalendar()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -30,4 +110,4 @@ async function copyText(e){
     }
 }
 
- shareButtons.forEach(shareButton=> shareButton.addEventListener('click', copyText))
+ 
